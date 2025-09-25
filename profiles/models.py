685630a691b371cb60b1a6bbe64e4a3dd82f86f6
nadiaglitch <nadia@nadiahossen.com>
2025-09-25 16:43:18 +0400

    Adding Link model

--- profiles/models.py
+++ profiles/models.py
@@ -12,3 +12,15 @@ class Profile(models.Model):
     profile_image = models.ImageField(upload_to="profiles/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Link(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="links")
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    sort_order = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["sort_order"]
