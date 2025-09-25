acb9dc01995f8fd55cbc6743ba849f3f54d7588f
nadiaglitch <nadia@nadiahossen.com>
2025-09-25 16:42:45 +0400

    Adding profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    handle = models.CharField(max_length=15, unique=True)
+    display_name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True)
+    profile_image = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
